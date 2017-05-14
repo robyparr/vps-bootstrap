901e25134d212c31f64949229194cf6d3ca5ea71
Roby Parr <roby.parr@gmail.com>
2017-05-14 17:31:15 +0900

    Fix spelling mistake.

--- bootstrap.sh
+++ bootstrap.sh
@@ -64,7 +64,7 @@ echo_formatted "Configure SSH..."
 sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 sed -i 's/#PasswordAuthentication no/PasswordAuthentication no/g' /etc/ssh/sshd_config
 systemctl reload sshd
-echo_success "SSL configured."
+echo_success "SSH configured."
 
 
 # Setup firewall
